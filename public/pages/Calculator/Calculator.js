f571a71d1e76fc6a7d1a8d019e4d56c31f12f49c
Jacob Melnick <jake.r.melnick@gmail.com>
2020-11-03 19:31:14 -0800

    calculator2

--- public/pages/Calculator/Calculator.js
+++ public/pages/Calculator/Calculator.js
@@ -1,14 +1,17 @@
-const cell1 = document.querySelector('#cell1')
-const cell2 = document.querySelector('#cell2')
-const cell3 = document.querySelector('#cell3')
-const cell4 = document.querySelector('#cell4')
-const cell5 = document.querySelector('#cell5')
-const cell6 = document.querySelector('#cell6')
-const cell7 = document.querySelector('#cell7')
-const cell8 = document.querySelector('#cell8')
-const cell9 = document.querySelector('#cell9')
-const cell0 = document.querySelector('#cell0')
-
+// HTML Input Selectors
+const cell1 = document.querySelector('#cell1');
+const cell2 = document.querySelector('#cell2');
+const cell3 = document.querySelector('#cell3');
+const cell4 = document.querySelector('#cell4');
+const cell5 = document.querySelector('#cell5');
+const cell6 = document.querySelector('#cell6');
+const cell7 = document.querySelector('#cell7');
+const cell8 = document.querySelector('#cell8');
+const cell9 = document.querySelector('#cell9');
+const cell0 = document.querySelector('#cell0');
+const equals = document.querySelector('#cell10');
+const penis = document.querySelector("#inputBox");
+const clear = document.querySelector("#clear")
 const numberCells = [
   cell1,
   cell2,
@@ -20,53 +23,43 @@ const numberCells = [
   cell8,
   cell9,
   cell0,
-]
-let number1;
-let number2;
-let answer;
+];
+
+// State:
+let numbers = [];
+
+const updateUI = () => {
+  penis.textContent = `${numbers.join(' + ')}`;
+}
 
 const handleCellClick = (event) => {
   const currentCell = event.target;
-  // If number1 is empty assign there
-  if (number1 === undefined){
-    number1 = parseInt(currentCell.value)
-  } else if(number2 === undefined) {
-    // if number1 is full assign to number2
-    number2 = parseInt(currentCell.value)
-  } else {
-    // if number1 and 2 are full calculate sum
-    answer = number1 + number2; 
-    alert(`${number1} + ${number2} = ${answer}`)
-    number1 = undefined;
-    number2 = undefined;
-  }
-  console.log('number1:', number1)
-  console.log('number2:', number2)
+  numbers.push(parseInt(currentCell.value))
+  console.log('numbers:', numbers)
+  updateUI()
 }
 
 numberCells.forEach((cell) => {
-  cell.addEventListener('click', handleCellClick)
+  cell.addEventListener('click', handleCellClick);
 })
 
-
-
-  
-
-
-  
-
-
-  const assignClickListenerToCell = (cells) => {
-    console.log("click")
+const handleEqualsClick = (event) => {
+  // sum of numbers
+  let answer = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    answer += numbers[i]; 
   }
+  numbers = [answer]
+  updateUI()
+}
 
+equals.addEventListener('click', handleEqualsClick);
 
+const handleClearClick = (event) => {
+  // if number1 and 2 are full calculate sum
+  answer = ''; 
+  numbers = []; //here is where we see the small fry mating with the big fry
+  updateUI()
+}
 
-
-
-
-
-
-  
-    
-    
+clear.addEventListener('click', handleClearClick);
